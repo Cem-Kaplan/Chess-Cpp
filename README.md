26c2f5a3eac0ecd58fe7f1f43779d937a63bca4c
Cem-Kaplan <cemk46908@gmail.com>
2026-01-03 13:04:14 +0100

    bewegen() angefangen

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Chess-Cpp
+
+linux:
+
+g++ src/main.cpp include/area.cpp -o main -I include -Iarea
--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,59 @@
 const std::string ANLEITUNG = "\n\n ! » Turm\n ? » Pferd\n % » läufer\n Q » Königing\n K » König\n b » bauer\n\n";
 CreateGame game;
 auto aktuelle_map = game.getGame();
+char figur;
 
 void zug() {
 
-        int y_input;
-        int x_input;
-        char figur = ','; // , bedeutet frei/nix
+    int y_input;
+    int x_input;
 
-        std::cout << "\nFigur aussuchen\nx:";
-        std::cin >> x_input;
-        x_input -= 1;
+    std::cout << "\nFigur aussuchen\nx:";
+    std::cin >> x_input;
 
-        std::cout << "y:";
-        std::cin >> y_input;
-        y_input -= 1;
+    std::cout << "y:";
+    std::cin >> y_input;
 
-        figur = aktuelle_map[y_input][x_input];
+    if (x_input < 1 || x_input > 8 || y_input < 1 || y_input > 8) {
+        std::cout << "Position ungültig" << std::endl;
+        return zug();
+    }
 
-        if (figur == ',') {
-            std::cout << "sie wählten einen ungültigen punkt aus" << std::endl;
-        } else {
-            std::cout << "Sie wählten aus: x: " << x_input << " y: " << y_input << "Figur: " <<  figur << std::endl;
-        }
+    y_input -= 1;
+    x_input -= 1;
 
+    figur = aktuelle_map.at(y_input).at(x_input);
+
+    if (figur == ',') {
+        std::cout << "sie wählten einen ungültigen punkt aus, nochmal versuchen" << std::endl;
+        return zug();
+    } else {
+        y_input += 1;
+        x_input += 1;
+        std::cout << "Sie wählten aus: x: " << x_input << " y: " << y_input << "Figur: " <<  figur << std::endl;
+    }
 }
 
-int main() {
-    std::cout << "Schach geladen" << ANLEITUNG;
+void bewegen() {
+    std::cout << "wohin bewegen?" << std::endl;
+    int bewegt_zu;
+    std::cin >> bewegt_zu;
 
-    bool status = true;
 
-    while (status) {
-        
-        std::cout << "nr _12345678_" << std::endl;
+    if (figur == 'b') {
+        if (bewegt_zu == 1) {
+            
+        } else {
+            return bewegen()
+        }
+    }
+}
 
-        for (int y = 0; y < aktuelle_map.size(); y++) {
+void map_zeigen() {
+
+    std::cout << "nr _12345678_" << std::endl;
+
+    for (int y = 0; y < aktuelle_map.size(); y++) {
             std::cout << y + 1 << ". |";
 
             for (int x = 0; x < aktuelle_map[y].size(); x++) {
@@ -49,6 +67,17 @@ int main() {
                 }
             }
         }
+}
+
+int main() {
+    std::cout << "Schach geladen" << ANLEITUNG;
+
+    bool status = true;
+
+    while (status) {
+        
+        map_zeigen();
+        
         zug();
     }
 
